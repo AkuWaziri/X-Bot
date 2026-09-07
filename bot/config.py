211e5475845a89c49bfbcac74915e0957ae36164
Aku Waziri <32820229+AkuWaziri@users.noreply.github.com>
2026-09-07 17:05:23 +0100

    Configure TwitterAPIs API key

--- bot/config.py
+++ bot/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
+TWITTERAPIS_API_KEY = os.getenv("TWITTERAPIS_API_KEY", "")
 
 CHECK_INTERVAL_MINUTES = int(os.getenv("CHECK_INTERVAL_MINUTES", "10"))
 MIN_REPLY_CHARS = int(os.getenv("MIN_REPLY_CHARS", "15"))
