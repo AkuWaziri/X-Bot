de9930ee0321d3706a6cc28ebf317c269667ca24
Aku Waziri <32820229+AkuWaziri@users.noreply.github.com>
2026-09-18 18:49:27 +0100

    Set maximum automatic reply length to 50 characters

--- bot/config.py
+++ bot/config.py
@@ -17,7 +17,7 @@ X_PROVIDER_MODE = os.getenv("X_PROVIDER_MODE", "twitterapis").strip().lower()
 
 CHECK_INTERVAL_MINUTES = int(os.getenv("CHECK_INTERVAL_MINUTES", "10"))
 MIN_REPLY_CHARS = int(os.getenv("MIN_REPLY_CHARS", "10"))
-MAX_REPLY_CHARS = int(os.getenv("MAX_REPLY_CHARS", "100"))
+MAX_REPLY_CHARS = int(os.getenv("MAX_REPLY_CHARS", "50"))
 MAX_REPLIES_PER_USER_PER_DAY = int(os.getenv("MAX_REPLIES_PER_USER_PER_DAY", "1"))
 AUTO_REPLY = os.getenv("AUTO_REPLY", "false").lower() == "true"
 
